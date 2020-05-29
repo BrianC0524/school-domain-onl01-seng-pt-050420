3e221e6a02d605f4cf7a9e06e12b6e2b053a000a
Brian Craycraft <craycraftb24@hotmail.com>
2020-05-29 08:50:23 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,47 @@
 # code here!
+class School
+  
+  attr_reader :name
+  
+  def initialize(name)
+    
+    @name = name
+    
+    @ROSTER = {}
+    
+  end
+  
+  def roster
+    
+    @ROSTER
+    
+  end
+  
+  def add_student(name, grade)
+    
+   if !@ROSTER.include?(grade)
+     @ROSTER[grade] = []
+   end
+   
+   @ROSTER[grade] << name
+    
+  end
+  
+  def grade(grade)
+    
+    @grade = grade
+    
+    @ROSTER[grade]
+    
+  end
+  
+  def sort
+    
+    
+    
+    @ROSTER.each {|grade, student| student.sort }.to_h
+    
+    
+  end
+  
+end
